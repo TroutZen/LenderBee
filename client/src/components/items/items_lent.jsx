1f8025c410ef18f0346d7805bec11837e1b46e32
Devin Otway <devin.otway@gmail.com>
2015-02-22 19:12:06 -0800

    (feat) fetch all items from server, filter items, and render items

--- client/src/components/items/items_lent.jsx
+++ client/src/components/items/items_lent.jsx
@@ -1,15 +1,30 @@
 var React = require('react');
+var _			= require('lodash');
 
-var Items_lent = React.createClass({
-
+var LentItem = React.createClass({
 	render: function() {
 		return (
 			<div>
 				{/* I want to be able to show lender information including avatar and name, so we need our endpoint to also fetch user data */}
-				<span>{this.props.item.title}</span>
-				<span>{this.props.item.description}</span>
-				<span>{this.props.item.pollenprice}</span>
-				<span>{this.props.item.borrower_id}</span>
+				<p>{this.props.item.title}</p>
+				<p>{this.props.item.description}</p>
+				<p>{this.props.item.pollenprice}</p>
+				<p>{this.props.item.borrower_id}</p>
+			</div>
+		);
+	}
+});
+
+var Items_lent = React.createClass({
+	render: function() {
+
+		var LentItems = _.map(this.props.item, function(item){
+			return <LentItem item={item}/>;
+		});
+
+		return (
+			<div>
+				{LentItems}
 			</div>
 		);
 	}
