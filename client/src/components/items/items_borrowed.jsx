69dd10a03c6bedb6253babc036b6118330dbfa4d
Devin Otway <devin.otway@gmail.com>
2015-03-06 20:25:12 -0800

    (feat/ui) items responsive just for borrowed not happy with how it looks

--- client/src/components/items/items_borrowed.jsx
+++ client/src/components/items/items_borrowed.jsx
@@ -5,26 +5,55 @@ var ItemBorrowed = React.createClass({
 
 	render: function(){
 
-		var inlineBlock = {
-			display: "inline-block"
+		// <div>
+		// 	<i className="circular gift icon"></i>
+		// 	<span>{this.props.item.title}</span>
+		// 	<div>
+		// 		<span>Description: {this.props.item.description}</span>
+		// 	</div>
+		// 	<div>
+		// 		<i className="circular money icon"></i>
+		// 		<span>{this.props.item.beebucks}</span>
+		// 	</div>
+		// 	<p>Lender: {this.props.item.lender_id}</p>
+		// </div>
+		// 
+		// var imgStyle = {
+		// 	maxHeight: "200px",
+		// 	overflow: "hidden",
+		// 	objectFit: "cover",
+		// }
+		
+		var relativeStyle = {
+			position: "relative"
+		};
+
+		var absolute = {
+			position: "absolute"
+		};
+
+		var topleft = {
+			top: "0",
+			left: "0"
 		};
 
 		return (
-			<div className="ui vertical segment">
-				{/* I want to be able to show lender information including avatar and name, so we need our endpoint to also fetch user data */}
-				<div>
-					<i className="circular gift icon"></i>
-					<span>{this.props.item.title}</span>
-					<div>
-						<span>Description: {this.props.item.description}</span>
+			<div className="column">
+				<div className="ui center aligned segment">
+					<div style={relativeStyle}>
+						<img className="ui bordered rounded medium image" src={this.props.item.imageurl}></img>
+						<div className="ui left aligned small segment">
+							<div style={absolute} style={topleft}>					
+								<i className="circular gift icon"></i>
+								<span>{this.props.item.title}</span>
+							</div>
+							<span>Description: {this.props.item.description}</span>
+							<i className="circular money icon"></i>
+							<span>{this.props.item.beebucks}</span>
+							<span>Lender: {this.props.item.lender_id}</span>
+						</div>
 					</div>
-					<div>
-						<i className="circular money icon"></i>
-						<span>{this.props.item.beebucks}</span>
-					</div>
-					<p>Lender: {this.props.item.lender_id}</p>
 				</div>
-				<img className="ui small left floated image" src={this.props.item.imageurl}></img>
 			</div>
 		);
 	}
@@ -33,6 +62,10 @@ var ItemBorrowed = React.createClass({
 
 var AllItemsBorrowed = React.createClass({
 
+		// <h2 className="ui horizontal header divider">
+	 //  	Currently Borrowed
+		// </h2>
+
 	render: function() {
 		// console.log('Items container with props', this.props.item);
 		var borrowedItems = _.map(this.props.item, function(item){
@@ -40,10 +73,7 @@ var AllItemsBorrowed = React.createClass({
 		});
 
 		return (
-			<div className="ui left aligned segment">
-				<h2 className="ui horizontal header divider">
-			  	Currently Borrowed
-				</h2>
+			<div className="ui centered stackable four column page grid">
 				{borrowedItems}
 			</div>
 		);
