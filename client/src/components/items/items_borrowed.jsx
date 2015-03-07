5df48a710255d8effb5ffd6360756fd5366f36d4
Devin Otway <devin.otway@gmail.com>
2015-03-07 10:55:24 -0800

    (feat/ui) borrowed items looking awesome, need to apply same style to lended and inventory

--- client/src/components/items/items_borrowed.jsx
+++ client/src/components/items/items_borrowed.jsx
@@ -5,57 +5,50 @@ var ItemBorrowed = React.createClass({
 
 	render: function(){
 
-		// <div>
-		// 	<i className="circular gift icon"></i>
-		// 	<span>{this.props.item.title}</span>
-		// 	<div>
-		// 		<span>Description: {this.props.item.description}</span>
-		// 	</div>
-		// 	<div>
-		// 		<i className="circular money icon"></i>
-		// 		<span>{this.props.item.beebucks}</span>
-		// 	</div>
-		// 	<p>Lender: {this.props.item.lender_id}</p>
-		// </div>
-		// 
-		// var imgStyle = {
-		// 	maxHeight: "200px",
-		// 	overflow: "hidden",
-		// 	objectFit: "cover",
-		// }
-		
-		var relativeStyle = {
-			position: "relative"
-		};
-
-		var absolute = {
-			position: "absolute"
-		};
-
-		var topleft = {
-			top: "0",
-			left: "0"
-		};
-
 		var lender = this.props.item.lender;
 
+		// <i className="circular tiny gift icon bee-fill"></i>
+		// <span>{this.props.item.title}</span>
+
+		// <i className="quote tiny left icon"></i>
+		// <span>{this.props.item.description}</span>
+		// <i className="quote tiny right icon"></i>			
+		var beeColor = {
+			backgroundColor: "#FFD34E"
+		}
+
+		var elementStyle = {
+		  position: "absolute",
+		  bottom: "27%",
+		  textAlign: "left",
+		  left: "7%",
+		  color: "white",
+		  backgroundColor: "black",
+		  opacity: "0.4",
+		  padding: "2%",
+		  borderRadius: "5px"
+		};
+
 		return (
-			<div className="column">
-				<div className="ui center aligned segment">
-					<div>
-						<img className="ui bordered rounded medium image" src={this.props.item.imageurl}></img>
-							<div>					
-								<i className="circular tiny gift icon"></i>
-								<span>{this.props.item.title}</span>
-								<i className="circular tiny money icon"></i>
-								<span>{this.props.item.beebucks}</span>
-								<i className="circular tiny user icon"></i>
-								<span>{lender.firstname + " " +lender.lastname}</span>
+				<div className="column">
+					<div className="ui segment">
+						<div>
+							<img className="ui fluid image" src={this.props.item.imageurl}></img>
+							<div style={elementStyle}>
+								<div>
+									<i className="tiny user icon"></i>
+									<span>{lender.firstname + " " +lender.lastname}</span>
+								</div>
+								<div>
+									<i className="tiny money icon"></i>
+									<span>{this.props.item.beebucks}</span>
+								</div>
 							</div>
-							<div>
-								<i className="quote tiny left icon"></i>
-								<span>Description: {this.props.item.description}</span>
-							</div>
+						</div>	
+						<div className="ui message" style={beeColor}>
+							<i className="quote tiny left icon"></i>
+							<span>{this.props.item.description}</span>
+							<i className="quote tiny right icon"></i>	
 						</div>
 					</div>
 				</div>
@@ -77,7 +70,7 @@ var AllItemsBorrowed = React.createClass({
 		});
 
 		return (
-			<div className="ui centered stackable four column page grid">
+			<div className="ui stackable four column grid">
 				{borrowedItems}
 			</div>
 		);
