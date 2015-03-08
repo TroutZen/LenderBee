ebe5cb0000c1ab54e4fe0275319409c5b7510cad
Devin Otway <devin.otway@gmail.com>
2015-03-07 22:24:53 -0800

    (fix) refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,11 @@ var watch = require('gulp-watch');
 var runSequence = require('run-sequence');
 var browserify = require('gulp-browserify');
 var run = require('gulp-run');
-var compass = require('gulp-compass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var clean = require('gulp-clean');
-var image = require('gulp-image');
+var nodemon = require('gulp-nodemon');
 
 
 var path = {
@@ -22,13 +21,13 @@ var path = {
     ASSETS: [__dirname + '/client/assets']
   },
   dest: {
-    OUT: 'lenderbee.js',
+    OUT: 'LenderBee.js',
     MINIFIED_OUT: 'lenderbee.min.js',
     DEST_SRC: 'client/dist',
     DEST_BUILD: 'client/dist/build',
     DEST: 'client/dist',
     DEST_ASSETS: 'client/dist/assets',
-    JS: 'client/dist/js'
+    JS: 'client/dist/src'
   },
   karmaConf: __dirname + '/karma.conf.js'
 };
@@ -36,10 +35,10 @@ var path = {
 // files to concat into final build
 // TODO: Needs to be updated with correct paths
 var filesToUglify = [
-  paths.src.bower + '/',
-  paths.src.bower + '/',
-  paths.src.bower + '/',
-  paths.dist.public + '/'
+  // paths.src.bower + '/',
+  // paths.src.bower + '/',
+  // paths.src.bower + '/',
+  // paths.dist.public + '/'
 ];
 
 
@@ -50,48 +49,59 @@ var handleError = function(err) {
 
 // TODO: file paths needs to be updated
 gulp.task('uglify', function() {
-  return gulp.src(filesToUglify)
-    .pipe(concat('lenderbee.js'))
-    .pipe(gulp.dest(paths.dist.js));
+  // return gulp.src(filesToUglify)
+  //   .pipe(concat('lenderbee.js'))
+  //   .pipe(gulp.dest(paths.dist.js));
 });
 
 // Cleans client/dist folder
 // TODO: confirm this is working...
 gulp.task('clean', function() {
-  return gulp.src(paths.dest.DEST)
+  return gulp.src(path.dest.DEST)
     .pipe(clean({
       force: true
     }))
     .on('error', handleError);   
 });
 
+// calls browserify task
 gulp.task('javascript', function(callback) {
-  runSequence('browserify', 'uglify', callback);
+  runSequence('browserify', callback);
 });
 
-// optimizes images
-gulp.task('image', function() {
-  return gulp.src(paths.sources.ASSETS + '/**/*')
-    .pipe(image())
-    .pipe(gulp.dest(paths.dest.DEST_ASSETS));
+// copies html/assets file into dist
+gulp.task('copy', function(){
+  gulp.src(path.sources.HTML, {base: 'client/'}).pipe(gulp.dest(path.dest.DEST));
+  gulp.src('client/assets/**/*').pipe(gulp.dest(path.dest.DEST_ASSETS));
 });
 
 // compiles jsx --> js
 gulp.task('browserify', function() {
-  return gulp.src(path.sources.JS)
+  return gulp.src(path.sources.ENTRY_POINT)
     .pipe(browserify({
       debug: false,
       transform: ['reactify'],
     }))
     .on('error', handleError)
-    .pipe(rename('lenderbee.bundled.js'))
-    .pipe(gulp.dest(paths.dest.JS))
+    .pipe(rename(path.dest.OUT))
+    .pipe(gulp.dest(path.dest.JS))
 });
 
 // paths to watch
 gulp.task('watch', function() {
-  gulp.watch(paths.sources.JS, ['javascript']);
+  gulp.watch(path.sources.JS, ['javascript']);
   gulp.watch(path.sources.ASSETS + '/**/*', ['image']);
+});
+
+// starts server and restarts on change
+gulp.task('nodemon', function() {
+  nodemon({
+    script: 'server/app.js',
+    env: {
+      'NODE_ENV': 'development'
+    }
+  })
+  .on('restart');
 });
 
 // Makes sure nodemon is run after build
@@ -101,9 +111,11 @@ gulp.task('server', function() {
 
 // deployment build
 gulp.task('build', function() {
-  runSequence('clean', 'javascript', 'image');
+  runSequence('clean', 'javascript');
 });
 
 // Default Task
-gulp.task('default', ['image', 'javascript', 'watch', 'server']);
+gulp.task('default', function(cb){
+  runSequence('copy', 'build', 'watch', 'server', cb)
+});
 
